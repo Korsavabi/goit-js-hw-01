5ebe7ec8f6966a2df3b47dfb686064d3106229c4
Bogdan Tkachenko <61245273+Korsavabi@users.noreply.github.com>
2020-09-03 15:48:50 +0300

    homeWork

---
+++ js/task-1-exercise.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let name,price;
+
+name = 'Генератор защитного поля';
+
+price = 'цена: 1000';
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
+
+price = 'цена: 2000';
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
